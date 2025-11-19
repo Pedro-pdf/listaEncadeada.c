49f857a1f788120f48485773088e579b39dcccc9
Pedro-pdf <pedrohenriquefrinhane@gmail.com>
2025-11-19 12:01:43 -0300

    Update listaEncadeada.c

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -1,153 +1,126 @@
-#include "lista.h"
+#include "listaEncadeada.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-Lista *criaLista()
-{
-    Lista *novaLista = malloc(sizeof(Lista));
+Lista* criaLista(){
+    Lista* novaLista = malloc(sizeof(Lista));
     novaLista->cabeca = NULL;
     return novaLista;
 }
 
-No *acessa(Lista *l, int i)
-{
-    No *no = l->cabeca;
+No* acessa(Lista* l, int i){
+    No* no = l->cabeca;
     int j = 0;
-    while (no->proximo != NULL && j != i)
-    {
+    while(no->proximo!=NULL && j!=i){
         no = no->proximo;
         j++;
     }
     return no;
 }
 
-No *busca(Lista *l, int chave)
-{
-    No *no = l->cabeca;
+No* busca(Lista* l, int chave){
+    No* no = l->cabeca;
     int j = 0;
-    while (no->proximo != NULL && no->chave != chave)
-    {
+    while(no->proximo != NULL && no->chave != chave){
         no = no->proximo;
         j++;
     }
-    if (no == NULL)
-    {
+    if(no == NULL){
         printf("Nao encontrado");
         return NULL;
     }
     return no;
 }
 
-void insere(Lista *l, int i, int valor)
-{
-    No *novo = malloc(sizeof(No));
-    novo->chave = valor;
-    if (i == 0)
-    {
+void insere(Lista* l, int i, int valor){
+    No* novo = malloc(sizeof(No));
+    novo -> chave = valor;
+    if(i == 0){
         novo->proximo = l->cabeca;
         novo->anterior = NULL;
         l->cabeca = novo;
-        if (novo->proximo != NULL)
-        {
+        if(novo->proximo != NULL){
             novo->proximo->anterior = novo;
         }
     }
-    else
-    {
+    else{
         int j = 1;
-        No *anterior = l->cabeca;
-        while (anterior != NULL && j < i)
-        {
+        No* anterior = l->cabeca;
+        while(anterior != NULL && j < i){
             anterior = anterior->proximo;
             j++;
         }
-        if (anterior == NULL)
-        {
+        if(anterior == NULL){
             printf("Indice invalido");
             return;
         }
         novo->proximo = anterior->proximo;
         novo->anterior = anterior;
         anterior->proximo = novo;
-        if (novo->proximo != NULL)
-        {
+        if(novo->proximo != NULL){
             novo->proximo->anterior = novo;
         }
     }
 }
 
-void removeValor(Lista *l, int valor)
-{
-    No *atual = l->cabeca;
-    No *anterior = NULL;
-    while (atual != NULL && atual->chave != valor)
-    {
+void removeValor(Lista* l, int valor){
+    No* atual = l->cabeca;
+    No* anterior = NULL;
+    while(atual != NULL && atual->chave != valor){
         anterior = atual;
         atual = atual->proximo;
     }
-    if (atual == NULL)
-    {
+    if(atual == NULL){
         printf("Valor nao encontrado");
         return;
     }
-    if (anterior == NULL)
-    {
+    if(anterior == NULL){
         l->cabeca = atual->proximo;
-        if (l->cabeca != NULL)
-        {
+        if(l->cabeca != NULL){
             l->cabeca->anterior = NULL;
         }
         return;
     }
-
+    
     anterior->proximo = atual->proximo;
-    if (atual->proximo != NULL)
-    {
+    if(atual->proximo != NULL){
         atual->proximo->anterior = anterior;
     }
     free(atual);
 }
 
-void removeIndice(Lista *l, int i)
-{
-    No *atual = l->cabeca;
-    No *anterior = NULL;
+void removeIndice(Lista* l, int i){
+    No* atual = l->cabeca;
+    No* anterior = NULL;
     int j = 0;
-    while (atual != NULL && j != i)
-    {
+    while(atual != NULL && j != i){
         anterior = atual;
         atual = atual->proximo;
         j++;
     }
-    if (atual == NULL)
-    {
+    if(atual == NULL){
         printf("Valor nao encontrado");
         return;
     }
-    if (anterior == NULL)
-    {
+    if(anterior == NULL){
         l->cabeca = atual->proximo;
-        if (l->cabeca != NULL)
-        {
+        if(l->cabeca != NULL){
             l->cabeca->anterior = NULL;
         }
         return;
     }
-
+    
     anterior->proximo = atual->proximo;
-    if (atual->proximo != NULL)
-    {
+    if(atual->proximo!=NULL){
         atual->proximo->anterior = anterior;
     }
     free(atual);
 }
-void imprime(Lista *l)
-{
-    No *nozes = l->cabeca;
-    while (nozes != NULL)
-    {
-        printf("%d ", nozes->chave);
-        nozes = nozes->proximo;
+void imprime(Lista * l){
+    No* aux = l->cabeca;
+    while(aux!=NULL){
+        printf("%d ",aux->chave);
+        aux = aux->proximo;
     }
     printf("\n");
 }
