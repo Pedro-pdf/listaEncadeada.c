d07f4d3f662a6e1136c240f95b9bbafaab380b01
Pedro-pdf <pedrohenriquefrinhane@gmail.com>
2025-11-18 23:21:39 -0300

    Update listaEncadeada.h

--- listaEncadeada.h
+++ listaEncadeada.h
@@ -6,16 +6,16 @@ typedef struct No{
     struct No* proximo;
     struct No* anterior;
 }No;
-
 typedef struct Lista{
     No* cabeca;
 }Lista;
 
-void criaFila();
+Lista* criaLista();
 No* acessa(Lista* l, int i);
 No* busca(Lista* l, int chave);
-void insere(Lista* l, int i);
+void insere(Lista* l, int i, int valor);
 void removeValor(Lista* l, int valor);
 void removeIndice(Lista* l, int i);
+void imprime(Lista * l);
 
 #endif
